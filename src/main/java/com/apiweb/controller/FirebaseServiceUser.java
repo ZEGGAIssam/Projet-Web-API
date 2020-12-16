c8461a17c3c3b0086ab5d0cd473b5bc663e00ace
Zeggai Issam <zeggai@et.esiea.fr>
2020-12-16 16:51:53 +0100

    Update des informations d'un user (version béta)

--- src/main/java/com/apiweb/controller/FirebaseServiceUser.java
+++ src/main/java/com/apiweb/controller/FirebaseServiceUser.java
@@ -1,9 +1,11 @@
 package com.apiweb.controller;
 
 import com.apiweb.model.User;
+import com.fasterxml.jackson.core.io.JsonStringEncoder;
 import com.google.api.core.ApiFuture;
 
 import com.google.cloud.firestore.*;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +17,21 @@ import static com.apiweb.var.*;
 
 @Service
 public class FirebaseServiceUser {
+    private static JsonStringEncoder FirebaseFirestore;
+
     static public String save(User user) throws ExecutionException, InterruptedException {
         Firestore dbFirestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection(USERS).document(user.getId()).set(user);
         return collectionsApiFuture.get().getUpdateTime().toString();
 
     }
+    public String updateUser(User user) throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("users").document(user.getFirstname()).set(user);
+        return collectionsApiFuture.get().getUpdateTime().toString();
 
-    static public User getConnected(String login) throws ExecutionException, InterruptedException {
+    }
+        static public User getConnected(String login) throws ExecutionException, InterruptedException {
         Firestore dbFireStore = FirestoreClient.getFirestore();
         List<QueryDocumentSnapshot> documentReference = dbFireStore.collection(USERS).whereEqualTo(LOGIN, login).get().get().getDocuments();
         if (documentReference.size() == 1)
