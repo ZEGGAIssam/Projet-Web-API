5074bcd571a3bece9adf3431cf10447a2831d320
Zeggai Issam <zeggai@et.esiea.fr>
2020-12-17 22:42:06 +0100

    bisous dana

--- target/classes/static/register.js
+++ target/classes/static/register.js
@@ -1,29 +1,25 @@
-$(document).ready(function(){
-    $("#registerSend").click(function(){
-        var username = $("#loginRegister").val().trim();
-        var password = $("#passwordRegister").val().trim();
-        var firstname = $("#firstnameRegister").val().trim();
-        var name = $("#nameRegister").val().trim();
+    $(document).ready(function(){
+        $("#registerSend").click(function(){
+            var username = $("#loginRegister").val().trim();
+            var password = $("#passwordRegister").val().trim();
+            var firstname = $("#firstnameRegister").val().trim();
+            var name = $("#nameRegister").val().trim();
 
-        if( username != "" && password != ""  && firstname != "" && name  != "" ){
-            $.ajax({
-                url:"http://localhost:8080/register",
-                type:POST,
-                data:JSON.stringify({name:name,firstname:firstname, username:username, password:password}),
-                success:function(response){
-                    var msg = "";
-                    console.log(response)
-                    if(response == 1){
-                        alert("let's go");
-                    }else{
-                        alert(response);
+            if( username != "" && password != ""  && firstname != "" && name  != "" ){
+                $.ajax({
+                    url:"http://localhost:8080/register",
+                    type:POST,
+                    data:JSON.stringify({name:name,firstname:firstname, username:username, password:password}),
+                    success:function(response){
+                        var msg = "";
+                        console.log(response)
+                        if(response == 1){
+                            alert("let's go");
+                        }else{
+                            alert(response);
+                        }
                     }
-                }
-            });
-        }
+                });
+            }
+        });
     });
-});
-$("#registerPage").click(function(){
-  window.location.href='meetingForm.html';
-  window.location.href='register.html';
-});
--- target/classes/static/updateuser.js
+++ target/classes/static/updateuser.js
@@ -1,27 +1,23 @@
 $(document).ready(function() {
     $("#updateuser").click(function () {
+        $.ajaxSetup({
+            contentType: "application/json; charset=utf-8"
+        });
 
-        var firstname = $("#firstnameRegister").val().trim();
-        var name = $("#nameRegister").val().trim();
+        var firstname = $("#firstnameUpdate").val().trim();
+        var name = $("#nameUpdate").val().trim();
 
-        let db;
-        db.collection = function (users) {
-            const userRef = db.collection("users");
-            userRef.update({name: true});
-            userRef.update({firstname: true});
-        }
-        url:"http://localhost:8080/updateuser",
+        $.ajax({
+            url: "http://localhost:8080/updateuser",
             type: POST,
             data: JSON.stringify({name: name, firstname: this.firstname}),
             success: function (response) {
-                var msg = "";
-                console.log(response)
                 if (response == 1) {
                     alert("User updated successfully !");
                 } else {
-                    alert(response);
+
                 }
             }
-        }
+        })
     });
 });
