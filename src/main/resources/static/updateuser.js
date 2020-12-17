06316637bc14298937dce747ee4e323e501b1e29
Zeggai Issam <zeggai@et.esiea.fr>
2020-12-17 19:24:39 +0100

    Updates

--- src/main/resources/static/updateuser.js
+++ src/main/resources/static/updateuser.js
@@ -1,13 +1,27 @@
-let db;
-db.collection = function (users) {
+$(document).ready(function() {
+    $("#updateuser").click(function () {
 
-    }
-        $(document).ready(function () {
-            $("#updateuser").click(function () {
+        var firstname = $("#firstnameRegister").val().trim();
+        var name = $("#nameRegister").val().trim();
 
-                const userRef = db.collection("users");
-                 userRef.update({name: true});
-                 userRef.update({firstname: true});
-
-            });
-        });
+        let db;
+        db.collection = function (users) {
+            const userRef = db.collection("users");
+            userRef.update({name: true});
+            userRef.update({firstname: true});
+        }
+        url:"http://localhost:8080/updateuser"{
+            type: POST,
+            data: JSON.stringify({name: name, firstname: this.firstname}),
+            success: function (response) {
+                var msg = "";
+                console.log(response)
+                if (response == 1) {
+                    alert("User updated successfully !");
+                } else {
+                    alert(response);
+                }
+            }
+        }
+    });
+});
