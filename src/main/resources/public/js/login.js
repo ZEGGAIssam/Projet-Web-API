c26fb553dc867a1677e0760e08eb97a2a6e051da
Zeggai Issam <zeggai@et.esiea.fr>
2020-12-18 12:49:43 +0100

    Ajout de library mvp.css pour habiller nos pages de façon intuitive Ajout de Toastify pour pop-up lors de l'ajout d'un user

--- src/main/resources/public/js/login.js
+++ src/main/resources/public/js/login.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+    const params= new URLSearchParams(window.location.search)
+    const registerSuccessParam=params.get("register_success")
+    console.log(registerSuccessParam)
+    if (registerSuccessParam) {
+        console.log(window.Toastify)
+        window.Toastify({
+            text: "User has been created successfully !"
+        }).showToast()
+// 1) import Toastify library Js sur notre page index.html = page de login
+        // importer le style de cet library .js - on a rediriger l'user sur la page index.html avec une query registersuccess (qui permet de verif que l'user a bien été crée)
+        // à l'arrivé de l'user sur cette page on vérifie que cette query existe, et si query=exist alors on a utilisé la library toastify pour afficher un msg
+        // query = valeur à une page pour que la page
+        // Ce sont des valeurs qu'on attribue à notre page HTML pour notre javascript
+        //$.notify("User has been updated successfully !")
+
+    }
     $("#loginSend").click(function(){
         var username = $("#usernameLogin").val().trim();
         var password = $("#passwordLogin").val().trim();
